72b99a6bfec6822c77942a2d55b4ae0915f1d6cf
Sunanda <2022wb86758@wilp.bits-pilani.ac.in>
2026-09-21 13:50:56 +0530

    Modified source code for CI test

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,6 +1,6 @@
 package com.example;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello 2022wb86758");
+        System.out.println("Hello World 2022wb86758");
     }
 }
